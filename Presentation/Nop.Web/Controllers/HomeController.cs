e01a833ca3d1b1875cfc5ee3b6bd7d88dcbf1a74
Sebastian Gurgitano <info.paperlst@gmail.com>
2019-03-03 13:05:26 +1300

    Actualizacion con codigo fuente

---
+++ Presentation/Nop.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Mvc.Filters;
+using Nop.Web.Framework.Security;
+
+namespace Nop.Web.Controllers
+{
+    public partial class HomeController : BasePublicController
+    {
+        [HttpsRequirement(SslRequirement.No)]
+        public virtual IActionResult Index()
+        {
+            return View();
+        }
+
+        public virtual IActionResult HomepageProducts(int items, int page)
+        {
+            return ViewComponent("HomepageProducts", new { items = items, page = page });
+        }
+    }
+}
